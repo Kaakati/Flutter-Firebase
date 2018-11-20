0992e6022844437cd35edfd5ef1ff03b331dca5d
Mohamad Kaakati <m@kaakati.me>
2018-11-20 15:49:39 +0300

    Add Tabs on top and slider with dummy data.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,16 +6,7 @@ enum HomePageType { list, ongoing, completed, more }
 class HomePage extends StatefulWidget {
   HomePage({this.onSignedOut});
   final VoidCallback onSignedOut;
-
-  void _signOut(BuildContext context) async {
-    try {
-      var auth = AuthProvider.of(context).auth;
-      await auth.signOut();
-      onSignedOut();
-    } catch (e) {
-      print(e);
-    }
-  }
+  String pageTitle;
 
   _HomePageState createState() => _HomePageState();
 }
@@ -34,16 +25,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: _buildNewTitle(context),
-        actions: <Widget>[
-          new FlatButton(
-            child: new Text('Logout',
-                style: new TextStyle(fontSize: 17.0, color: Colors.white)),
-            onPressed: () => widget._signOut(context),
-          )
-        ],
-      ),
+      
       body: new Center(child: _buildNewBody(context)),
       bottomNavigationBar: new BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
@@ -106,9 +88,7 @@ class _HomePageState extends State<HomePage> {
   Widget _buildNewBody(BuildContext context) {
     switch (_homePageType) {
       case HomePageType.list:
-        return new Container(
-          child: new Text('List Tab'),
-        );
+        return new ListPage(pageTitle: 'List', onSignedOut: widget.onSignedOut,);
       case HomePageType.ongoing:
         return new Container(
           child: new Text('Ongoing Tab'),
@@ -122,5 +102,105 @@ class _HomePageState extends State<HomePage> {
           child: new Text('More Tab'),
         );
     }
+  }
+}
+
+
+class ListPage extends StatefulWidget {
+  
+  ListPage({this.pageTitle, this.onSignedOut});
+  String pageTitle;
+  final VoidCallback onSignedOut;
+
+  void _signOut(BuildContext context) async {
+    try {
+      var auth = AuthProvider.of(context).auth;
+      await auth.signOut();
+      onSignedOut();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _ListPageState createState() => _ListPageState();
+
+}
+
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: new Text(widget.pageTitle),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('Logout',
+                style: new TextStyle(fontSize: 17.0, color: Colors.white)),
+                onPressed: () => widget._signOut(context),
+              )
+            ],
+            bottom: TabBar(
+              isScrollable: true,
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        ),
+      );
+  }
+}
+
+class Choice {
+  const Choice({this.title, this.icon});
+
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Appetizers', icon: Icons.directions_car),
+  const Choice(title: 'Breakfast', icon: Icons.directions_bike),
+  const Choice(title: 'Lunch', icon: Icons.directions_boat),
+  const Choice(title: 'Platters', icon: Icons.directions_bus),
+  const Choice(title: 'Sandwiches', icon: Icons.directions_railway),
+  const Choice(title: 'Drinks', icon: Icons.directions_walk),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display1;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
+      ),
+    );
   }
 }
